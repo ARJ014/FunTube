580ee61eb4b44681a7f06800bbaccc7b53b0d45a
ARJ014 <arjungoel014@gmail.com>
2023-06-04 02:12:46 +0530

    Completed the backend of teh app and also started with the frontend

---
+++ server/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import userRouter from "./routes/users.js";
+import videoRouter from "./routes/video.js";
+import commentRouter from "./routes/comment.js";
+import authRouter from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((e) => {
+    throw e;
+  });
+
+//middleware
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+//routes
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/video", videoRouter);
+app.use("/api/comment", commentRouter);
+
+//error handling
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Error has occured";
+  return res.status(status).json({
+    status,
+    message,
+  });
+});
+
+app.listen(8800, () => {
+  console.log(`Listening on server `);
+});
